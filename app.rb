c67c1115f4903456ce5921a860350704acf71cf0
Dylan Manley <dylan.manley@gmail.com>
2019-12-20 12:36:19 -0800

    create definition view and add funtionality

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra/reloader')
 require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
+require('./lib/definition')
 
 get('/') do
   @songs = Word.all
@@ -10,20 +11,30 @@ get('/') do
 end
 
 get('/words') do
-  @@words = Word.all
+  if params["clear"]
+    @albums = Album.clear()
+  else
+    @@words = Word.all
+  end
   erb(:words)
 end
 
 get('/words/new') do
-
-end
-
-get ('/words/:id') do
-
+  erb(:new_word)
 end
 
 post('/words') do
+  name = params[:word_name]
+  pronunciation = params[:pronunciation_in]
+  word = Word.new(name, pronunciation, nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
 
+get ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
 end
 
 get('/words/:id/edit') do
