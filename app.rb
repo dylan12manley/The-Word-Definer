7c5ac6d97e7991239dbc2edc10e002278dc3bef2
Dylan Manley <dylan.manley@gmail.com>
2019-12-20 10:12:36 -0800

    add attributes hash

--- app.rb
+++ app.rb
@@ -3,3 +3,13 @@ require('sinatra/reloader')
 require('./lib/definer')
 require('pry')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @songs = Word.all
+  erb(:words)
+end
+
+get('/word') do
+  @words = Word.all
+  erb(:words)
+end
--- lib/definer.rb
+++ lib/definer.rb
@@ -4,6 +4,7 @@ class Word
   @@total_rows = 0
 
   def initialize(attributes)
+    @name = attributes.fetch(:name)
     @definition = attributes.fetch(:definition)
     @id = attributes.fetch(:id) || @@total_rows += 1
   end
@@ -13,8 +14,10 @@ class Word
   end
 
   def save(attributes)
-    @@word[self.id] = Word.new(self.definition, self.id)
+    @@word[self.id] = Word.new(attributes)
+    @name = attributes.fetch(:name)
+    @definition = attributes.fetch(:definition)
+    @id
   end
-
 
 end
