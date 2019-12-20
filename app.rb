28c1619cbabb1df5cddb8857919e8c0ed4c1c093
Dylan Manley <dylan.manley@gmail.com>
2019-12-20 10:57:52 -0800

    create all routes needed for project in app.rb

--- app.rb
+++ app.rb
@@ -9,7 +9,31 @@ get('/') do
   erb(:words)
 end
 
-get('/word') do
+get('/words') do
   @words = Word.all
   erb(:words)
+end
+
+get('/words/new') do
+
+end
+
+get ('/words/:id') do
+
+end
+
+post('/words') do
+
+end
+
+get('/words/:id/edit') do
+
+end
+
+patch('/words/:id') do
+
+end
+
+delete('/albums/:id') do
+
 end
