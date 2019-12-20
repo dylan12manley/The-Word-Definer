6f028aac56cf29e51ad4509f2e225a8d1bb851ca
Dylan Manley <dylan.manley@gmail.com>
2019-12-20 13:04:05 -0800

    add test for save method

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,6 +1,23 @@
 require 'rspec'
 require 'word'
-# 
-# describe '#Word' do
-#
+
+describe '#Word' do
+
+# describe('#==') do
+#   it("is the same word if it has the same attributes as another word") do
+#       word = Word.new("Blue", nil, 1)
+#       word2 = Word.new("Blue", nil, 1)
+#       expect(word).to(eq(word2))
 # end
+# end
+describe('#save') do
+    it("saves an word") do
+      word = Word.new("Giant Steps", nil, nil) 
+      word.save()
+      word2 = Word.new("Blue", nil, nil)
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
+
+end
