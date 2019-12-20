5929d3cca6940f46dcf12ee521087833ebf53807
Dylan Manley <dylan.manley@gmail.com>
2019-12-20 13:59:50 -0800

    add routing for definitions

--- app.rb
+++ app.rb
@@ -49,9 +49,35 @@ patch('/words/:id') do
   erb(:words)
 end
 
-delete('/albums/:id') do
+delete('/words/:id') do
   @word = Word.find(params[:id].to_i())
   @word.delete()
   @words = Word.all
   erb(:words)
+end
+
+get('/words/:id/definition/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
+post('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_name], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
+patch('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.update(params[:name], @word.id)
+  erb(:word)
+end
+
+delete('/words/:id/definitions/:definition_id') do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
 end
