8a3b5af692287bf3dd8d50cfba38575710cbd792
Dylan Manley <dylan.manley@gmail.com>
2019-12-22 21:46:56 -0800

    add intigration spec for definition and attempted to change the program to actually add definitions

--- app.rb
+++ app.rb
@@ -68,15 +68,16 @@ end
 
 post('/words/:id/definitions') do
   @word = Word.find(params[:id].to_i())
-  definition = Definition.new(params[:definition_in], @word.id, nil)
+  definition = Definition.new(params[:definition_name], @word.id, nil)
   definition.save()
   erb(:word)
 end
 
+
 patch('/words/:id/definitions/:definition_id') do
   @word = Word.find(params[:id].to_i())
   definition = Definition.find(params[:definition_id].to_i())
-  definition.update(params[:name], @word.id)
+  definition.update(params[:definition_name], @word.id)
   erb(:word)
 end
 
--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,13 +3,23 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-
 describe('create a word path', {:type => :feature}) do
-it('creates a word and goes to that word page') do
-  visit('/words')
-  click_on('Add a new word')
-  fill_in('word_name', :with => 'tim')
-  click_on('Go!')
-  expect(page).to have_content('tim')
+  it('creates a word and goes to that word page') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'tim')
+    click_on('Go!')
+    expect(page).to have_content('tim')
+  end
 end
-end
+
+# describe('create a definition path', {:type => :feature}) do
+#   it('creates a word with a definition and then goes to the word page') do
+#     word = Word.new("Kitty", nil, nil,)
+#     word.save
+#     visit("/words/#{word.id}")
+#     fill_in('definition_name', :with => 'issa cat')
+#     click_on('Add definition')
+#     expect(page).to have_content('issa cat')
+#   end
+# end
