c5da98a9e183a71159592a4bda70865498fc9732
Dylan Manley <dylan.manley@gmail.com>
2019-12-27 14:56:05 -0800

    add styling, change paramater definition to name in definition.rb

--- lib/definition.rb
+++ lib/definition.rb
@@ -5,8 +5,8 @@ class Definition
   @@definitions = {}
   @@total_rows = 0
 
-  def initialize(definition, word_id, id)
-    @definition = definition
+  def initialize(name, word_id, id)
+    @name = name
     @word_id = word_id
     @id = id || @@total_rows += 1
   end
