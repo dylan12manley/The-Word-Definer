abde7ee0ef892f6096fde182988e4057d3a34e6a
Dylan Manley <dylan.manley@gmail.com>
2020-01-12 17:13:27 -0800

    create integration test for clear words, and comment in test for creates a definition path, get tests to pass

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -13,13 +13,24 @@ describe('create a word path', {:type => :feature}) do
   end
 end
 
-# describe('create a definition path', {:type => :feature}) do
-#   it('creates a word with a definition and then goes to the word page') do
-#     word = Word.new("Kitty", nil, nil,)
-#     word.save
-#     visit("/words/#{word.id}")
-#     fill_in('definition_name', :with => 'issa cat')
-#     click_on('Add definition')
-#     expect(page).to have_content('issa cat')
-#   end
-# end
+describe('clears a word paths', {:type => :feature}) do
+  it('creates a word and deleats that word') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'tim')
+    click_on('Go!')
+    click_on('Clear All Words')
+    expect(page).to have_content('There are currently no words to display.')
+  end
+end
+
+describe('create a definition path', {:type => :feature}) do
+  it('creates a word with a definition and then goes to the word page') do
+    word = Word.new("Kitty", nil, nil,)
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('definition_name', :with => 'issa cat')
+    click_on('Add definition')
+    expect(page).to have_content('issa cat')
+  end
+end
