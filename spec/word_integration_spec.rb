750fc9225d4126ea54353b0ed0939fe62d685c71
Dylan Manley <dylan.manley@gmail.com>
2020-01-12 17:23:14 -0800

    make intergration tests pass

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -13,13 +13,17 @@ describe('create a word path', {:type => :feature}) do
   end
 end
 
-describe('clears word paths', {:type => :feature}) do
-  it('clears words and goes to that word page') do
+describe('clears all word paths', {:type => :feature}) do
+  it('creates a word and deleats that word') do
     visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'tim')
+    click_on('Go!')
     click_on('Clear All Words')
     expect(page).to have_content('There are currently no words to display.')
   end
 end
+
 
 describe('create a definition path', {:type => :feature}) do
   it('creates a word with a definition and then goes to the word page') do
